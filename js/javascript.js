773387dfc3534fab81be5fb7a4c80749b9e90242
Omar Einea <eineao@gmail.com>
2016-01-06 00:00:39 +0300

    Add login dialog and many tweaks

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-      $('.slider').slider({
-			full_width: false,
-			indicators: true,
-			height: 500,
-			transition: 400,
-			interval: 4000
-		});
+	$('.slider').slider({
+		full_width: false,
+		indicators: true,
+        height: 550,
+		transition: 400,
+		interval: 4000
 	});
+	
+	$('.modal-trigger').leanModal({
+		dismissible: true,
+		opacity: 0.2,
+		in_duration: 350,
+		out_duration: 250,
+    });
+});
+
+function loggedIn() {
+	$('#login-dialog').closeModal();
+	if(username.value === '' || password.value === '')
+		Materialize.toast('Missing Username or Password', 4000)
+	else
+		Materialize.toast('Logged in as ' + username.value, 5000)
+}
